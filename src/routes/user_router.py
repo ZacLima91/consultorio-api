19f0f710c2889a8f9fdac6eaa40cbd18f6b8bb49
zaclima9 <jadielsonmanoel@gmail.com>
2023-05-24 17:53:01 -0300

    created the user's create, show and delete routes

---
+++ src/routes/user_router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+from src.infra.sqlalchemy.config.database import get_db
+from src.schemas.schemas import UserBase
+from src.infra.sqlalchemy.repository.user import RepositoryUser
+
+
+router = APIRouter()
+
+
+@router.post("/user", status_code=status.HTTP_201_CREATED, response_model=UserBase)
+async def create_user(user: UserBase,session: Session = Depends(get_db)):
+    created_user = await RepositoryUser.create_user(user)
+    return created_user
+
+@router.get("/user", status_code=status.HTTP_200_OK, response_model=List[UserBase])
+async def get_all_users(session: Session = Depends(get_db)):
+    users = await RepositoryUser.get_all_user()
+    return users
+
+@router.delete("/user", status_code=status.HTTP_200_OK)
+async def delete_user(user_id: int, session: Session = Depends(get_db)):
+    deleted_user = await RepositoryUser.delete_user(user_id)
+    return deleted_user
