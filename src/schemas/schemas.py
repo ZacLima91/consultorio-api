bb735e0b8cca2f8a91923521c4a2a2bde1ebea6f
zaclima9 <jadielsonmanoel@gmail.com>
2023-05-24 16:54:05 -0300

    started the virtual environment and created the User schema

---
+++ src/schemas/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class UserBase(BaseModel):
+    id: Optional[int] = None
+    username: str
+    email: Optional[str]
+    cpf: str
+    password: str
+    laudos: Optional[List[str]] = None
+    role = bool = True
+
+    class Config:
+        orm_mode = True
+
+
